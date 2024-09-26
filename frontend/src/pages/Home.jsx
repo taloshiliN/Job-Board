43a95bf9ccd16ed8566d1b2a5648e8518b9e928c
Taloshili Nghiwewelekwa <nghiwewelekwataloshili@gmail.com>
2024-09-26 21:49:40 +0200

    workwave

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,14 +7,25 @@ function Home() {
       <TopNavigation />
       <div className='Header'>
         <div className='Top'>
-          <h1 className='WorkWave'>WorkWave</h1>
+          <h2 className='WorkWave'>WorkWave</h2>
+          <h2>Brand Profile</h2>
+          <h2>Check Profiles</h2>
+          <h2>Sign In</h2>
         </div>
         <div className='Info'>
+          <h1>Welcome to 
+            WorkWave!!!
+          </h1>
           There's suppose to be words here
+        </div>
+        <div className='header-buttons'>
+          <button>Request a demo</button>
+          <button>Post a Job</button>
         </div>
       </div>
       <div className='Benefits'>
-        Benefits
+        <h2>Unlock the benefits of WorkWave</h2>
+        <h3>Elevate Your Hiring Strategy to New Heights with Workwave's comprehensive solutions</h3>
       </div>
       <div className='Articles'>
         Articles
